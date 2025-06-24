3dd21ce4eddbca4df8bed26d06e6c6bca44938f1
Allyson <loera.allyson6120@gmail.com>
2025-06-24 14:01:59 -0600

    Intento de conexion a render

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const Contacto = require('./models/Contacto');
 require('./mongo');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors());
